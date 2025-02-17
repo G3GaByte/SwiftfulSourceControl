7931c265ead9cd2c67d7a9d5986e75eb0a943edc
Omar Said <ostsaid@hotmail.com>
2025-02-17 16:01:41 +0200

    [Clean] update docs

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -7,15 +7,19 @@
 
 /*
  
- Clone = Copying the repo locally
- Commit = save ("checkpoint") changes on our current branch
- Staging = prepare changes for a commit
- Stash = save personal changes for later
- Push = Send local commits to remote repo
- Pull = Fetch remote commmits to local repo
- Merge = Joining two different branches
- Rebase = Moving one branch on top of another branch
- Cherry picking = Duplicating (copying) one commit from one branch to another
+ Clone              = Copying the repo locally
+ Commit             = save ("checkpoint") changes on our current branch
+ Staging            = prepare changes for a commit
+ Stash              = save personal changes for later
+ Push               = Send local commits to remote repo
+ Pull               = Fetch remote commmits to local repo
+ Merge              = Joining two different branches
+ Rebase             = Moving one branch on top of another branch
+ 
+ Cherry picking     = Duplicating (copying) one commit from one
+ branch to another
+ 
+ Pull Request (PR)  = Request to merge branch
  
  
  
